473da6a58e06e6b98ab22d9d89cab9f4bb35443d
Sarah Barnekow <mellamopepe@gmail.com>
2014-10-07 21:52:36 -0700

    added autoform and simpleschema to create my article schema/views quickly and easily :)

--- client/store.js
+++ client/store.js
@@ -1,10 +1,19 @@
+ArticleSchema = new SimpleSchema({
+	title: {
+		type: String,
+		label: "Title",
+		max: 200
+	},
+	description: {
+		type: String,
+		label: "Description",
+		max: 2000
+	},
+	price: {
+		type: Number,
+		label: "Price",
+		min: 0
+	}
+});
 Articles = new Mongo.Collection("articles");
-
-Meteor.subscribe("articles");
-
-Articles.insert({name: "Random Test Article"});
-
-var article_list = Articles.find().fetch();
-
-console.log( Meteor.userId() );
-console.log(article_list);
+Articles.attachSchema(ArticleSchema);
