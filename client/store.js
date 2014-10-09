2ba918a4e11b715124e77e9150cf3f03f8993b96
Sarah Barnekow <mellamopepe@gmail.com>
2014-10-08 22:40:58 -0700

    fixed rendering for new items in the store

--- client/store.js
+++ client/store.js
@@ -1,7 +1,8 @@
+SimpleSchema.debug = true;
 ArticleSchema = new SimpleSchema({
 	title: {
 		type: String,
-		label: "Title",
+	    label: "Title",
 		max: 200
 	},
 	description: {
@@ -13,11 +14,12 @@ ArticleSchema = new SimpleSchema({
 		type: Number,
 		label: "Price",
 		min: 0
-	},
-	fileId: {
-		type: String,
-		label: "Picture"
 	}
+	// fileId: {
+	// 	type: String,
+	// 	label: "fileId",
+	// 	optional: true
+	// }
 });
 Articles = new Mongo.Collection("articles");
 Articles.attachSchema(ArticleSchema);
@@ -25,3 +27,9 @@ Articles.attachSchema(ArticleSchema);
 Files = new FS.Collection("files", {
 	stores: [new FS.Store.GridFS("filesStore")]
 });
+
+Template.articles.Articles = function(){
+	// Articles.insert({title: "hello", description: "this is a description", price: 10});
+	console.log('im getting read');
+	return Articles.find({});
+};
